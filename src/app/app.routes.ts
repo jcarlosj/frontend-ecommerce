1d734997800a553c567b9b58558b72871d586cc8
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-10-02 12:14:22 -0500

    [+] page-not-found.component: Pagina no encontrada [+] header.component*: Crea enlaces de navegacion del sitio usando routerLink & routerLinkActive [+] app.routes: Define las rutas de navegacion y redirecciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,15 @@ import { ProductsComponent } from './pages/products/products.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'product-detail', component: ProductDetailComponent },
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
-  { path: '**', redirectTo: '' }
+  { path: '404', component: PageNotFoundComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: '404', pathMatch: 'full' }
 ];
