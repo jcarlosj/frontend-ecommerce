c7998fd27596c67899e20ed259129180ccabf096
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-10-03 12:46:25 -0500

    [+] Instala dependencia @fortawesome [+] header.component*: Despliega Logo e Iconos (FontAwesome)

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCartShopping, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ RouterLink, RouterLinkActive ],
+  imports: [ RouterLink, RouterLinkActive, FontAwesomeModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  faCartShopping = faCartShopping;
+  faBars = faBars;
+  faXmark = faXmark;
 
 }
