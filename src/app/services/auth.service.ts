6124fef9b472cc3b22d4782e90a9d1052727c6af
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-12-04 19:52:21 -0500

    [+] auth.service.ts: Implementa RxJS a registerUser

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,25 @@ export class AuthService {
   constructor( private http: HttpClient ) {}
 
   registerUser( newUser: any ) {
-    return this.http.post( 'http://localhost:4000/api/auth/register', newUser );
+    return this.http.post<any>( 'http://localhost:4000/api/auth/register', newUser )
+      .pipe(
+        // tap( ( data: any ) => {
+        //   console.log( 'tap ', data );
+        // }),
+        map( ( data: any ) => {
+          return 'Registro realizado exitosamente';
+        }),
+        catchError( ( err ) => {
+
+          // Error cuando el usuario a registrar ya existe
+          if( err.error.msg ) {
+            return of( err.error.msg );
+          }
+
+          // Error cuando el endpoint que vamos a acceder no existe en nuestra API
+          return of( 'Error: El servidor esta fallando' );
+        })
+      );
   }
 
   loginUser( credentials: any ) {
