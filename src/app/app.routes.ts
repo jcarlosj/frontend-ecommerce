460f4019c36f812aab82c88031875cdd15067a2a
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2025-02-03 14:23:11 -0500

    [+] app.routes.ts: propiedad data a cada ruta con la lista de roles esperados por ruta [+] header.component.html: Visualiza el rol del usuario autenticado [+] role.guard.ts: Define funcionalidad del Guard de Autorizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,11 @@ import { PageNotFoundComponent } from './pages/public/page-not-found/page-not-fo
 import { RegisterComponent } from './pages/public/register/register.component';
 import { LoginComponent } from './pages/public/login/login.component';
 import { DashboardComponent } from './pages/private/dashboard/dashboard.component';
+import { CategoriesComponent } from './pages/private/categories/categories.component';
 
 import { authGuard } from './guards/auth.guard';
-import { CategoriesComponent } from './pages/private/categories/categories.component';
+import { roleGuard } from './guards/role.guard';
+
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -17,9 +19,24 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: '404', component: PageNotFoundComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [ authGuard ] },
-  { path: 'dashboard/products', component: ProductsComponent, canActivate: [ authGuard ] },
-  { path: 'dashboard/categories', component: CategoriesComponent, canActivate: [ authGuard ] },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [ authGuard, roleGuard ],
+    data: { expectedRoles: [ 'registered', 'moderator', 'admin' ] }
+  },
+  {
+    path: 'dashboard/products',
+    component: ProductsComponent,
+    canActivate: [ authGuard, roleGuard ],
+    data: { expectedRoles: [ 'moderator', 'admin' ] }
+  },
+  {
+    path: 'dashboard/categories',
+    component: CategoriesComponent,
+    canActivate: [ authGuard, roleGuard ],
+    data: { expectedRoles: [ 'admin' ] }
+  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: '404', pathMatch: 'full' }
 ];
--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
 
 export const roleGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject( AuthService );
+  const router = inject( Router );
+
+  const expectedRoles = route.data[ 'expectedRoles' ];
+  const userData = authService.userData;
+
+  console.log( expectedRoles, userData?.role );
+
+  const isAuthorized = expectedRoles.includes( userData?.role );
+
+  if( ! isAuthorized ) {
+    router.navigateByUrl( 'dashboard' );
+  }
+
+  return isAuthorized;
 };
