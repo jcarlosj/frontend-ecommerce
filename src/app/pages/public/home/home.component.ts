c51c2950a3fc38e8e7eb4aa4a24e9a26bcbcafd4
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2025-05-03 17:13:03 -0500

    [+] home.component.ts: Obtiene todos los productos a traves de la inyeccion de la dependencia productsService en el ciclo de vida de inicializacion del componente [+] home.component.html: Itera la cantidad de productos obtenidos y muestra cada uno de ellos con su respectiva informacion dentro de un card

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../../services/products.service';
+import { DataProduct } from '../../../models/product.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ CurrencyPipe ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  products: DataProduct[] | undefined;
+
+  constructor( private productsService: ProductsService ) {}
+
+  ngOnInit() {
+    /** Obtener todos los productos del backend */
+    this.productsService.getProducts().subscribe({
+      next: ( data ) => {
+        console.log( data );
+
+        this.products = data.data;
+      },
+      error: ( error ) => {
+        console.error( error );
+      },
+      complete: () => {}
+    });
+  }
 
 }
